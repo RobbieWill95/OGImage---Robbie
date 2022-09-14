00e1f1852a165c852b72736a8c0526d45c35b1b0
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-09-14 14:22:35 -0500

    changes

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -31,88 +31,85 @@ function getCss(theme: string, fontSize: string) {
     foreground = 'white'
     radial = 'dimgray'
   }
-  return `
-    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;700&display=swap');
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
-    }
-
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
-    }
-
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
-
-    body {
-        background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
-        height: 100vh;
-        display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
-    }
-
-    code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
-    }
-
-    code:before, code:after {
-        content: '\`';
-    }
-
-    .logo-wrapper {
-        display: none;
-        text-align: left;
-        // align-items: center;
-        // align-content: center;
-        // justify-content: center;
-        // justify-items: center;
-    }
-
-    .logo {
-        margin: 0 75px;
-    }
-
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
-    }
-
-    .spacer {
-        margin: 150px;
-    }
-
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
-    }
-    
-    .heading {
-        font-family: 'Noto Sans SC', 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
-    }`
+  return `@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;700&display=swap");
+  @font-face {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: normal;
+    src: url(data:font/woff2;charset=utf-8;base64,bold) format("woff2");
+  }
+  @font-face {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: bold;
+    src: url(data:font/woff2;charset=utf-8;base64,bold) format("woff2");
+  }
+  @font-face {
+    font-family: "Vera";
+    font-style: normal;
+    font-weight: normal;
+    src: url(data:font/woff2;charset=utf-8;base64,mono}) format("woff2");
+  }
+  body {
+    background: #000;
+    background-image: radial-gradient(
+        circle at 25px 25px,
+        dimgray 2%,
+        transparent 0%
+      ),
+      radial-gradient(circle at 75px 75px, dimgray 2%, transparent 0%);
+    background-size: 100px 100px;
+    height: 100vh;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+  }
+  code {
+    color: #d400ff;
+    font-family: "Vera";
+    white-space: pre-wrap;
+    letter-spacing: -5px;
+  }
+  code:before,
+  code:after {
+    content: "\`";
+  }
+  .logo-wrapper {
+    display: block;
+    text-align: center;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    justify-items: center;
+  }
+  .logo {
+    margin: 0 75px;
+  }
+  .plus {
+    color: #bbb;
+    font-family: Times New Roman, Verdana;
+    font-size: 100px;
+  }
+  .spacer {
+    margin: 25px;
+  }
+  
+  .heading {
+    font-family: "Noto Sans SC", "Inter", sans-serif;
+    font-size: 4rem;
+    font-style: normal;
+    margin-top: 0;
+    color: #fff;
+  }
+  
+  .credit {
+      font-family: "Noto Sans SC", "Inter", sans-serif;
+    font-size: 1rem;
+    font-style: normal;
+    margin-top: 3rem;
+    color: #fff;
+  }`
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
@@ -123,27 +120,26 @@ export function getHtml(parsedReq: ParsedRequest) {
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        ${getCss(theme, fontSize)}
+    ${getCss(theme, fontSize)}
     </style>
     <body>
         <div>
             <div class="spacer">
             <div class="logo-wrapper">
-                ${images
-                  .map(
-                    (img, i) =>
-                      getPlusSign(i) + getImage(img, widths[i], heights[i])
-                  )
-                  .join('')}
+              <img src="https://blog.clark.today/_next/image?url=%2Fme_macbook.png&w=96&q=75" alt="">
             </div>
             <div class="spacer">
-            <div class="heading">${emojify(
+            <div class="heading">
+            ${emojify(
               md ? marked(text) : sanitizeHtml(text)
             )}
             </div>
+              <div class="credit">Blog.Clark.Today/</div>
         </div>
     </body>
-</html>`
+</html>
+
+`
 }
 
 function getImage(src: string, width = 'auto', height = '225') {
